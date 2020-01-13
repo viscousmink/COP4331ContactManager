c5dfde4aafb0a0aae5efc5cae92593c02fcd81a7
Mitchell Klingler <MessierObject99@Seans-MacBook-Pro.local>
2020-01-13 10:54:16 -0500

    adding cosmosdb to the server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoClient = require("mongodb").MongoClient;
+mongoClient.connect("mongodb://viscousmink:03VnXo6H953S1Ixpw5H35vYG5auG1XfCrg7y9Of6l87JdGCCQux1gDGP9AlEw9DpoVg0ykzY0BrT4ZmtzunLwg%3D%3D@viscousmink.documents.azure.com:10255/?ssl=true", function (err, client) {
+  client.close();
+});
 
 const app = express()
 app.use(bodyParser.json())
