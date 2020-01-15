b79ade1244b1099559c7dd2361684f644d6b9121
Mitchell Klingler <MessierObject99@Seans-MacBook-Pro.local>
2020-01-15 13:16:43 -0500

    Cosmos Works

--- server.js
+++ server.js
@@ -3,14 +3,13 @@ const express = require('express')
 const bodyParser = require('body-parser')
 //const database = require('./database.js');
 
-const CosmosClient = require('@azure/cosmos').CosmosClient;
+const mongoClient = require('mongodb').MongoClient;
 
-const mongoClient = new CosmosClient("mongodb://viscousmink:03VnXo6H953S1Ixpw5H35vYG5auG1XfCrg7y9Of6l87JdGCCQux1gDGP9AlEw9DpoVg0ykzY0BrT4ZmtzunLwg==@viscousmink.documents.azure.com:10255/?ssl=true&replicaSet=globaldb")
-
-//var mongoClient = require("mongodb").MongoClient;
-//mongoClient.connect("mongodb://viscousmink:03VnXo6H953S1Ixpw5H35vYG5auG1XfCrg7y9Of6l87JdGCCQux1gDGP9AlEw9DpoVg0ykzY0BrT4ZmtzunLwg%3D%3D@viscousmink.documents.azure.com:10255/?ssl=true", function (err, client) {
-//  client.close();
-//})
+mongoClient.connect('mongodb://viscousmink:03VnXo6H953S1Ixpw5H35vYG5auG1XfCrg7y9Of6l87JdGCCQux1gDGP9AlEw9DpoVg0ykzY0BrT4ZmtzunLwg==@viscousmink.documents.azure.com:10255/?ssl=true&replicaSet=globaldb', function (err, db) {
+  if(!err) {
+  	console.log('Connected');
+  }
+ });
 
 const app = express()
 app.use(bodyParser.json())
@@ -33,9 +32,3 @@ const itemID = {
 	name: 'firstName',
 	lastName: 'lastName'
 	}
-
-const { database } = mongoClient.databases.createIfNotExists({ id: databaseID });
-
-const { container } = mongoClient.database(databaseId).containers.createIfNotExists({ id: containerID });
-
-const { item } = client.database(databaseId).container(containerId).items.create(item);
