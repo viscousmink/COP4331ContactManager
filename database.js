45b2f6f92fb58e8d39ac09dfb0cc7fcd98510270
Mitchell Klingler <MessierObject99@Seans-MacBook-Pro.local>
2020-01-15 14:28:07 -0500

    refactoring

--- database.js
+++ database.js
@@ -1,21 +1,19 @@
-const mongoose = require('mongoose');
-const server = '127.0.0.1:27017'
-const database = 'initialDB'
-
+const mongoClient = require('mongodb').MongoClient;
+const Db = require('mongodb').Db;
 class Database {
   constructor() {
     this._connect()
   }
   
 _connect() {
-     mongoose.connect(`mongodb://${server}/${database}`)
-       .then(() => {
-         console.log('Database connection successful')
-       })
-       .catch(err => {
-         console.error('Database connection error')
-       })
-  }
+    	mongoClient.connect('mongodb://viscousmink:03VnXo6H953S1Ixpw5H35vYG5auG1XfCrg7y9Of6l87JdGCCQux1gDGP9AlEw9DpoVg0ykzY0BrT4ZmtzunLwg==@viscousmink.documents.azure.com:10255/?ssl=true&replicaSet=globaldb', function (err, client) {
+	 		if(!err) {
+	  			console.log('Connected');
+	  			var db = client.db('startingout');
+	  			console.log('DB name is : ' + db.databaseName);
+			}
+		});
+	}
 }
 
 module.exports = new Database()
--- server.js
+++ server.js
@@ -1,29 +1,9 @@
-require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser')
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
 //const database = require('./database.js');
 
-const mongoClient = require('mongodb').MongoClient;
-const Db = require('mongodb').Db;
-
-//test code
-const databaseID = 'firstDBID';
-const containerID = 'firstCID';
-const itemID = {
-	id: 'TestOut.1',
-	country: 'USA',
-	name: 'firstName',
-	lastName: 'lastName'
-	}
-
-
-mongoClient.connect('mongodb://viscousmink:03VnXo6H953S1Ixpw5H35vYG5auG1XfCrg7y9Of6l87JdGCCQux1gDGP9AlEw9DpoVg0ykzY0BrT4ZmtzunLwg==@viscousmink.documents.azure.com:10255/?ssl=true&replicaSet=globaldb', function (err, client) {
-  if(!err) {
-  	console.log('Connected');
-  	var db = client.db('startingout');
-  	console.log('DB name is : ' + db.databaseName);
-  }
- });
+const database = require('./database.js');
 
 const app = express()
 app.use(bodyParser.json())
