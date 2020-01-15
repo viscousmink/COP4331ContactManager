9870a1e1e1d28b517b77d7329d77cc61b032d84e
Mitchell Klingler <MessierObject99@Seans-MacBook-Pro.local>
2020-01-15 14:34:47 -0500

    insertion work

--- database.js
+++ database.js
@@ -1,16 +1,26 @@
 const mongoClient = require('mongodb').MongoClient;
 const Db = require('mongodb').Db;
 class Database {
-  constructor() {
-    this._connect()
-  }
-  
-_connect() {
+	constructor() {
+		this._connect()
+	}
+
+	insert(db) {
+		db.collection('startingout').insertOne( {
+			"id": "Something"
+		}, function(err, result) {
+			assert.equal(err, null);
+			console.log('Inserted a document into the db');
+		});
+	}
+
+	_connect() {
     	mongoClient.connect('mongodb://viscousmink:03VnXo6H953S1Ixpw5H35vYG5auG1XfCrg7y9Of6l87JdGCCQux1gDGP9AlEw9DpoVg0ykzY0BrT4ZmtzunLwg==@viscousmink.documents.azure.com:10255/?ssl=true&replicaSet=globaldb', function (err, client) {
 	 		if(!err) {
 	  			console.log('Connected');
 	  			var db = client.db('startingout');
 	  			console.log('DB name is : ' + db.databaseName);
+	  			insert(db)
 			}
 		});
 	}
