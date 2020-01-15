faa8a13295f9cc8ae2d39956f6b643602db0c144
Mitchell Klingler <MessierObject99@Seans-MacBook-Pro.local>
2020-01-15 15:15:29 -0500

    Working

--- database.js
+++ database.js
@@ -42,9 +42,9 @@ class Database {
     	mongoClient.connect('mongodb://viscousmink:03VnXo6H953S1Ixpw5H35vYG5auG1XfCrg7y9Of6l87JdGCCQux1gDGP9AlEw9DpoVg0ykzY0BrT4ZmtzunLwg==@viscousmink.documents.azure.com:10255/?ssl=true&replicaSet=globaldb', function (err, client) {
 	 		if(!err) {
 	  			console.log('Connected');
-	  			var db = client.db('startingout');
+	  			var db = client.db('startingoutnoProvThrough');
 	  			console.log('DB name is : ' + db.databaseName);
-	  			db.collection('startingout').insertOne({"id": "Something"}, function(err, result) {
+	  			db.collection('noProvThrough').insertOne({"id": "Something"}, function(err, result) {
     				if (err) throw err;
     				console.log(result);
   					});
