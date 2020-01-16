6b0297379a017b697330830abc88f12df7cd91c3
Mitchell Klingler <MessierObject99@Seans-MacBook-Pro.local>
2020-01-15 19:09:17 -0500

    tabs

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 
 app.get('/*', (req, res) => {
-  res.sendFile('index.html', { root: __dirname});
+	res.sendFile('index.html', { root: __dirname});
 });
 
 const PORT = process.env.PORT;
