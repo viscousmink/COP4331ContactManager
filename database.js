846ea44ba281e8c2307e0b38364f7833634e2895
Mitchell Klingler <MessierObject99@Seans-MacBook-Pro.local>
2020-01-16 12:56:19 -0500

    Revert "working on API" Yup This reverts commit 8f38dfaa84070d3e96b4bec795553b03e9f5bce9.

--- database.js
+++ database.js
@@ -1,9 +1,41 @@
 const mongoClient = require('mongodb').MongoClient;
 const Db = require('mongodb').Db;
+const insert = function(db) {
+	db.collection('startingout').insertOne( {
+		"id": "Something"
+	}, function(err, result) {
+		if(err == null) {
+			console.log('Inserted a document into the db');
+		} else {
+			console.log('failed insertion');
+		}
+	});
+}
+
+var insertDocument = function(db, callback) {
+db.collection('families').insertOne( {
+        "id": "AndersenFamily",
+        "lastName": "Andersen",
+        "parents": [
+            { "firstName": "Thomas" },
+            { "firstName": "Mary Kay" }
+        ],
+        "children": [
+            { "firstName": "John", "gender": "male", "grade": 7 }
+        ],
+        "pets": [
+            { "givenName": "Fluffy" }
+        ],
+        "address": { "country": "USA", "state": "WA", "city": "Seattle" }
+    }, function(err, result) {
+    console.log("Inserted a document into the families collection.");
+    callback();
+});
+};
 
 class Database {
 	constructor() {
-		this._connect('startingoutnoProvThrough', {"random": "random"});
+		this._connect()
 	}
 
 	_connect() {
@@ -12,28 +44,13 @@ class Database {
 	  			console.log('Connected');
 	  			var db = client.db('startingoutnoProvThrough');
 	  			console.log('DB name is : ' + db.databaseName);
-	  			//db.collection('noProvThrough').insertOne({"id": "Something"}, function(err, result) {
-    			//	if (err) throw err;
-    			//	console.log(result);
-  				//	});
+	  			db.collection('noProvThrough').insertOne({"id": "Something"}, function(err, result) {
+    				if (err) throw err;
+    				console.log(result);
+  					});
 	  			client.close();
 			}
 		});
-	}
-	_connect(dbName, JSON) {
-		mongoClient.connect('mongodb://viscousmink:03VnXo6H953S1Ixpw5H35vYG5auG1XfCrg7y9Of6l87JdGCCQux1gDGP9AlEw9DpoVg0ykzY0BrT4ZmtzunLwg==@viscousmink.documents.azure.com:10255/?ssl=true&replicaSet=globaldb'), function(err, client) {
-			if(!err) {
-				console.log('Connected');
-				var db = client.db('startingoutnoProvThrough');
-				console.log('DB name is : ' + db.databaseName);
-				db.collection('noProvThrough').insertOne(JSON, function(err, result) {
-					if(err) throw err;
-					console.log(result);
-				})
-
-				client.close();
-			}
-		};
 	}
 }
 
