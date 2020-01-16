ade8d4e703fdfbc6d43cb465bcfbb1a881980c85
Mitchell Klingler <MessierObject99@Seans-MacBook-Pro.local>
2020-01-16 13:01:51 -0500

    beginning the API

--- database.js
+++ database.js
@@ -1,41 +1,9 @@
 const mongoClient = require('mongodb').MongoClient;
 const Db = require('mongodb').Db;
-const insert = function(db) {
-	db.collection('startingout').insertOne( {
-		"id": "Something"
-	}, function(err, result) {
-		if(err == null) {
-			console.log('Inserted a document into the db');
-		} else {
-			console.log('failed insertion');
-		}
-	});
-}
-
-var insertDocument = function(db, callback) {
-db.collection('families').insertOne( {
-        "id": "AndersenFamily",
-        "lastName": "Andersen",
-        "parents": [
-            { "firstName": "Thomas" },
-            { "firstName": "Mary Kay" }
-        ],
-        "children": [
-            { "firstName": "John", "gender": "male", "grade": 7 }
-        ],
-        "pets": [
-            { "givenName": "Fluffy" }
-        ],
-        "address": { "country": "USA", "state": "WA", "city": "Seattle" }
-    }, function(err, result) {
-    console.log("Inserted a document into the families collection.");
-    callback();
-});
-};
 
 class Database {
 	constructor() {
-		this._connect()
+		this._connect('startingoutnoProvThrough', 'noProvThrough', {"id": "Something"})
 	}
 
 	_connect() {
@@ -45,6 +13,21 @@ class Database {
 	  			var db = client.db('startingoutnoProvThrough');
 	  			console.log('DB name is : ' + db.databaseName);
 	  			db.collection('noProvThrough').insertOne({"id": "Something"}, function(err, result) {
+    				if (err) throw err;
+    				console.log(result);
+  					});
+	  			client.close();
+			}
+		});
+	}
+
+	_connect(dbName, collection, JSON) {
+		mongoClient.connect('mongodb://viscousmink:03VnXo6H953S1Ixpw5H35vYG5auG1XfCrg7y9Of6l87JdGCCQux1gDGP9AlEw9DpoVg0ykzY0BrT4ZmtzunLwg==@viscousmink.documents.azure.com:10255/?ssl=true&replicaSet=globaldb', function (err, client) {
+			if(!err) {
+	  			console.log('Connected');
+	  			var db = client.db(dbName);
+	  			console.log('DB name is : ' + db.databaseName);
+	  			db.collection(collection).insertOne(JSON, function(err, result) {
     				if (err) throw err;
     				console.log(result);
   					});
