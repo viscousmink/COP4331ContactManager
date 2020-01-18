e3553e441060b44b34c6c735d6546750b5c39bdf
Mitchell Klingler <MessierObject99@Seans-MacBook-Pro.local>
2020-01-18 10:34:48 -0500

    mistyped the file path

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 //const database = require('./database.js');
 
 const database = require('./database.js');
-const contact = require('./contact.js');
+const contact = require('./API/models/contact.js');
 
 const app = express()
 app.use(bodyParser.json())
