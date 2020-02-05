ea72992adf039bd0667122bafdc973a906dfdf6d
Mitchell Klingler <MessierObject99@Seans-MacBook-Pro.local>
2020-02-05 11:56:41 -0500

    leaned out server.js

--- server.js
+++ server.js
@@ -4,23 +4,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const database = require('./database.js');
 
-
-/*mongoose.connect(database.URL, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, db) {
-    if (err) {
-        console.log('Unable to connect to the server. Please start the server. Error:', err);
-    } else {
-        console.log('Connected to Server successfully!');
-    }
-}); */
-
-//const listener = require('./API/listener.js');
-
 const app = express();
-/*app.use(function(req, res, next) {
-    const error = new Error('Not found.');
-    error.status = 404;
-    next(error);
-}); */
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
