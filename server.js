2188498901d922f04637a49d138d78438121157b
Arthur Henry <arthuradvtech@icloud.com>
2020-02-09 13:51:46 -0500

    test: testing heroku express deployment

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const database = require('./database.js');
+// const mongoose = require('mongoose');
+// const database = require('./database.js');
 const path = require('path');
 
 const app = express();
@@ -11,22 +11,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api', require('./API/app.js'));
 
-//app.use(express.static(path.join(__dirname, "frontend", "build")));
+// Serve any static files
+app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
+// Handle React routing, return all requests to React app
 app.get('/*', (req, res) => {
 	res.sendFile('/frontend/public/index.html', { root: __dirname });
 	//rex.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
 });
 
-if (process.env.NODE_ENV == 'production') {
-	// Serve any static files
-	app.use(express.static(path.join(__dirname, 'client/build')));
+// const PORT = process.env.PORT;
+// app.listen(PORT, () => console.log('Wizardous stuff on ' + PORT));
 
-	// Handle React routing, return all requests to React app
-	app.get('*', function(req, res) {
-		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-	});
-}
+const port = process.env.PORT || 5000;
+app.listen(port);
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => console.log('Wizardous stuff on ' + PORT));
+console.log(`Wizardous stuff on ${port}`);
