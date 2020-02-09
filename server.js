2945658bb8afe11714b5661e7a169f2c77bc0d98
Arthur Henry <arthuradvtech@icloud.com>
2020-02-09 13:27:47 -0500

    test: testing heroku deployment

--- server.js
+++ server.js
@@ -8,15 +8,25 @@ const path = require('path');
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api', require('./API/app.js'));
 
 //app.use(express.static(path.join(__dirname, "frontend", "build")));
 
 app.get('/*', (req, res) => {
-	res.sendFile('/frontend/public/index.html', { root: __dirname});
+	res.sendFile('/frontend/public/index.html', { root: __dirname });
 	//rex.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
 });
+
+if (process.env.NODE_ENV == 'production') {
+	// Serve any static files
+	app.use(express.static(path.join(__dirname, 'client/build')));
+
+	// Handle React routing, return all requests to React app
+	app.get('*', function(req, res) {
+		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+	});
+}
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log('Wizardous stuff on ' + PORT));
