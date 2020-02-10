181204aebc39c419b5eebd997342de620022c411
Mitchell Klingler <MessierObject99@Seans-MBP.attlocal.net>
2020-02-09 20:20:16 -0500

    working database con

--- database.js
+++ database.js
@@ -1,12 +1,10 @@
 require('dotenv').config();
 
-const PASSWORD = process.env.PASSWORD;
-const USER = process.env.USER;
+const { PASSWORD } = process.env;
 
 module.exports = {
-<<<<<<< HEAD
-    URL : 'mongodb+srv://viscousmink:' + PASSWORD + '@cluster0-sb44x.azure.mongodb.net/test?retryWrites=true&w=majority'
-=======
-    URL : 'mongodb+srv://' + USER + ':' + PASSWORD + '@cluster0-sb44x.azure.mongodb.net/test?retryWrites=true&w=majority'
->>>>>>> c17571e22778f6b25e083d4f73e95d9ddc6931d9
+	URL:
+		'mongodb+srv://viscousmink:' +
+		PASSWORD +
+		'@cluster0-sb44x.azure.mongodb.net/test?retryWrites=true&w=majority'
 };
