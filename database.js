95ad88ec291e8330ce5bf6c6c7e81fd666f5f7e2
Mitchell Klingler <MessierObject99@Seans-MBP.attlocal.net>
2020-02-09 19:28:25 -0500

    database adding user

--- database.js
+++ database.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 
-const { PASSWORD } = process.env;
+const PASSWORD = process.env.PASSWORD;
+const USER = process.env.USER;
 
 module.exports = {
-    URL : 'mongodb+srv://viscousmink:' + PASSWORD + '@cluster0-sb44x.azure.mongodb.net/test?retryWrites=true&w=majority'
+    URL : 'mongodb+srv://' + USER + ':' + PASSWORD + '@cluster0-sb44x.azure.mongodb.net/test?retryWrites=true&w=majority'
 };
