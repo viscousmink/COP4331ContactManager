f360d88365c41a47b7211d23eb4a0c2016e68e6e
Arthur Henry <arthuradvtech@icloud.com>
2020-02-09 20:02:55 -0500

    test: hopefully works now

--- database.js
+++ database.js
@@ -4,13 +4,5 @@ const PASSWORD = process.env.PASSWORD;
 const USER = process.env.USER;
 
 module.exports = {
-<<<<<<< HEAD
-    URL : 'mongodb+srv://' + USER + ':' + PASSWORD + '@cluster0-sb44x.azure.mongodb.net/test?retryWrites=true&w=majority'
+	URL: `mongodb+srv://${USER}:${PASSWORD}@cluster0-sb44x.azure.mongodb.net/test?retryWrites=true&w=majority`
 };
-=======
-	URL:
-		'mongodb+srv://viscousmink:' +
-		PASSWORD +
-		'@cluster0-sb44x.azure.mongodb.net/test?retryWrites=true&w=majority'
-};
->>>>>>> 0c8fd4720a7fc9d502d6f96ee6b6c41c78c8a6a6
